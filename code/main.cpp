f62016adaeb41dbe58dfb01b66ebde65c6a51ead
Brendon Brewer <brendon.brewer@gmail.com>
2015-03-30 17:34:29 -0400

    Load the data file from here

--- code/main.cpp
+++ code/main.cpp
@@ -27,7 +27,7 @@ using namespace DNest3;
 
 int main(int argc, char** argv)
 {
-	Data::get_instance().load("/scratch/daniela/data/sgr1550/dnest/unbinned/090122283_+096.17100_eventfile.dat");
+	Data::get_instance().load("090122283_+096.17100_eventfile.dat");
 	MTSampler<MyModel> sampler = setup_mt<MyModel>(argc, argv);
 	sampler.run();
 	return 0;
