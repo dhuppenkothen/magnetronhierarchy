4cb91764c76b98622a74874ae00ab2981c08dbf7
Brendon Brewer <brendon.brewer@gmail.com>
2015-03-31 12:25:10 -0400

    Plot the data and the model

--- code/display.py
+++ code/display.py
@@ -1,17 +1,20 @@
 from pylab import *
+import numpy as np
 
 data = np.loadtxt('090122283_+096.17100_eventfile.dat')
 posterior_sample = atleast_2d(loadtxt('posterior_sample.txt'))
 
 ion()
+hold(False)
 for i in xrange(0, posterior_sample.shape[0]):
-  hold(False)
-  plot(data[:,0], data[:,1], "-", color='black', lw=1)
+  counts, bin_edges = np.histogram(data[:,0], 100)
+  dt = bin_edges[1]-bin_edges[0]
+  countrate = counts/dt
+  plot(bin_edges[:-1]+dt/2., countrate, color="black")
   hold(True)
   plot(data[:,0], posterior_sample[i, -data.shape[0]:], 'r-')
-  ylim([0, 1.1*data[:,1].max()])
+  ylim([0, 1.1*countrate.max()])
   draw()
-
 ioff()
 show()
 
