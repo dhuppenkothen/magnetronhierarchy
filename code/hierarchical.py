fb128a98af2ca5475aed748fd52afe04d6741b6d
Brendon Brewer <brendon.brewer@gmail.com>
2015-05-12 17:33:42 +1200

    Attempt at log marginal likelihood for 1D problem

--- code/hierarchical.py
+++ code/hierarchical.py
@@ -11,7 +11,7 @@ def get_samples():
   Returns a list of 2D numpy arrays. Each element
   of the list is the posterior samples for one burst.
   """
-  num_bursts = 20
+  num_bursts = 3
   num_params = 1   # Number of within-burst hyperparameters
   samples = []
 
@@ -20,9 +20,31 @@ def get_samples():
 
   return samples
 
+def log_likelihood(mu, samples):
+  """
+  Log of the marginal likelihood for the hyperparameters
+  The marginal likelihood is:
+  p(data | alpha) = \int p(theta | alpha) p(data | theta) dtheta
+                  = Z \int p(theta | alpha)/pi(theta) p(data | theta) pi(theta)/Z dtheta
+  """
+  logL = 0.
+  for i in range(len(samples)):
+    pi = 1./200
+    integrand = np.exp(-0.5*(samples[i][0] - mu)**2)/pi
+    logL += np.log(np.mean(integrand))
+
+  return logL
+
+
+
 
 if __name__ == '__main__':
+  # Generate or load samples
   samples = get_samples()
-  plt.hist(samples[0][:,0], 100)
+
+  for i in range(0, len(samples)):
+    plt.hist(samples[i][:,0], 100, alpha=0.2)
   plt.show()
+
+  print(log_likelihood(0., samples))
 
